2968853f084c5c18409279ab6bb402597815aef2
NikitaKrutsenKo <niankr5@gmail.com>
2024-05-09 17:25:03 +0300

    fix_cssToView_problem

--- djangoCICD/settings.py
+++ djangoCICD/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from os.path import join
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,9 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIR = [
-    join(BASE_DIR, 'static')
+    BASE_DIR / 'static'
 ]
 
 
