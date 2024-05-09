c372e87e0fd580e30db159e3284e98a0b84851f3
NikitaKrutsenKo <niankr5@gmail.com>
2024-05-09 23:32:20 +0300

    fix_front_problem

--- djangoCICD/urls.py
+++ djangoCICD/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from home.views import home_page
+from main.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page, name='home')
+    path('', home, name='home')
 ]
