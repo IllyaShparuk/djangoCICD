85ed3fc5687a6eb3a7d5e95befd408357a97b2ce
NikitaKrutsenKo <niankr5@gmail.com>
2024-05-12 17:40:02 +0300

    models_game_developer

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Game, GameDeveloper
 
-# Register your models here.
+
+@admin.register(GameDeveloper)
+class GameDeveloperAdmin(admin.ModelAdmin):
+    list_display = ('id', 'developer_name', 'developer_photo', 'ref_to_social')
+    list_editable = ('developer_name', 'developer_photo', 'ref_to_social')
+    
+
+
+@admin.register(Game)    
+class GameAdmin(admin.ModelAdmin):    
+    list_display = ('id', 'game_name', 'price', 'rating', 'genre', 'release_date', 'developer', 'game_photo')
+    list_editable = ('game_name', 'price', 'rating', 'genre', 'release_date', 'developer', 'game_photo')
+
--- main/models.py
+++ main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class GameDeveloper(models.Model):
+    developer_name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    developer_photo = models.ImageField(upload_to='dev_photo/', blank=True, null=True)
+    ref_to_social = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return self.developer_name
+
+
+class Game(models.Model):
+    game_name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    rating = models.DecimalField(validators=[MaxValueValidator(5), MinValueValidator(0)], max_digits=20, decimal_places=2)
+    genre = models.CharField(max_length=50)
+    release_date = models.DateField()
+    developer = models.ForeignKey(GameDeveloper, on_delete= models.CASCADE, related_name='games')
+    game_photo = models.ImageField(upload_to='games_photo/', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.game_name
+    
+        
+
+
+    
