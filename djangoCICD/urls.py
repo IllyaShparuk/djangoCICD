31496de80e4614cf2a3d1b5b3df33cd4a2eacef7
Yuk1ra <159479985+Stanislav192@users.noreply.github.com>
2024-05-16 14:31:42 +0300

    Commit_SortGameByDate

--- djangoCICD/urls.py
+++ djangoCICD/urls.py
@@ -23,6 +23,7 @@ from main.views import home
 from account.views import sign_up
 from cart.views import add_to_cart, open_cart, delete_from_cart
 from user_games_lib.views import add_items_to_lib, open_user_lib
+from main.views import game_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,7 +34,8 @@ urlpatterns = [
     path('open_cart', open_cart, name='open_cart'),
     path('delete_from_cart', delete_from_cart, name='delete_from_cart'),
     path('add_items_to_lib', add_items_to_lib, name='add_items_to_lib'),
-    path('open_user_lib', open_user_lib, name='open_user_lib')
+    path('open_user_lib', open_user_lib, name='open_user_lib'),
+    path('game_list', game_list, name='game_list')
 ]
 
 
--- main/views.py
+++ main/views.py
@@ -6,5 +6,13 @@ def home(request):
     games = Game.objects.all()
 
     return render(request, 'main.html', context={
-        'games' : games
+        'games': games
     })
+
+def game_list(request):
+    sort_order = request.GET.get('sort')
+    if sort_order == 'desc':
+        games = Game.objects.all().order_by('-release_date')
+    else:
+        games = Game.objects.all().order_by('release_date')
+    return render(request, 'main.html', {'games': games, 'sort_order': sort_order})
