5a7ed6131d511152ff062484d59100fd07d0c409
Yuk1ra <159479985+Stanislav192@users.noreply.github.com>
2024-05-17 00:56:31 +0300

    Added_TestSearchGame

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Game, GameDeveloper
 
-# Create your tests here.
+class GameSearchTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.developer = GameDeveloper.objects.create(
+            developer_name='Test Developer',
+            slug='test-developer',
+            ref_to_social='http://example.com'
+        )
+        cls.game1 = Game.objects.create(
+            game_name='Game 1',
+            slug='game-1',
+            price=9.99,
+            rating=4.5,
+            genre='Action',
+            release_date='2023-05-17',
+            developer=cls.developer,
+            game_photo='http://example.com/game1.jpg'
+        )
+        cls.game2 = Game.objects.create(
+            game_name='Game 2',
+            slug='game-2',
+            price=14.99,
+            rating=4.0,
+            genre='Adventure',
+            release_date='2022-12-31',
+            developer=cls.developer,
+            game_photo='http://example.com/game2.jpg'
+        )
+
+    # Перевіряємо, як працює функція пошуку ігор, коли у нас є пошуковий запит
+    def test_game_search_with_query(self):
+        response = self.client.get(reverse('game_search'), {'q': 'Game 1'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Game 1')
+        self.assertNotContains(response, 'Game 2')
+
+
+
