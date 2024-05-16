4e875bf1224ad529556be1cf96e50843074d04e3
IllyaShparuk <illya.shparuk@knu.ua>
2024-05-16 16:44:29 +0300

    HOTFIX cookie deletion problem

--- user_games_lib/views.py
+++ user_games_lib/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import GameLibItem, UserGamesLib
 from cart.models import Cart, CartItem
 from cart.views import clear_cart
 
 def add_items_to_lib(request):
+    if not request.user.is_active:
+        return redirect('/login')
     user_cart = Cart.objects.get(user=request.user)
     cart_items = CartItem.objects.filter(cart=user_cart)
     games = [cart_item.product for cart_item in cart_items]
@@ -21,6 +23,8 @@ def add_items_to_lib(request):
 
 
 def open_user_lib(request):
+    if not request.user.is_active:
+        return redirect('/login')
     user_lib, _ = UserGamesLib.objects.get_or_create(user=request.user)
     user_games_list = GameLibItem.objects.filter(lib=user_lib)
     return render(request, 'user_lib.html', context={'user_games_list' : user_games_list})
